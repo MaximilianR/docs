d130d35619f8da41c4cc79dc200d2f1529f3f17f
mo <moanonresearch@gmail.com>
2025-11-26 14:26:42 +0100

    popup: warning

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import FeedbackButton from '@site/src/components/FeedbackButton';
+import DevelopmentBanner from '@site/src/components/DevelopmentBanner';
 
 export default function Root({ children }) {
   return <>
+    <DevelopmentBanner />
     {children}
     <FeedbackButton />
   </>;
